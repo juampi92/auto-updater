a3f9db6f0a1b320c76f3e4d09f0b2400afbed34a
Juan Pablo <juampi92@gmail.com>
2015-12-30 13:36:01 -0300

    Revert "added a missing gitcheck method from 0.1.0 branch"

--- auto-updater.js
+++ auto-updater.js
@@ -192,9 +192,7 @@ var commands = {
    */
   'check': function() {
     // first check git if needed
-    if (this.attrs.checkgit && checkGit.call(this)){
-      return;
-    }
+    if (this.attrs.checkgit && checkGit.call(this)) return;
 
     loadClientJson.call(this)
       .then(loadRemoteJson.bind(this))
@@ -290,24 +288,6 @@ var parsePackageJson = function(data) {
   return filtered;
 };
 
-/**
- * Fires:
- *   'git-clone' if it has a .git folder
- * @method _checkGit
- * @return {Boolean}  Has git folder
- * @private
- */
-var checkGit = function() {
-  if (this.cache.git === undefined) {
-
-    this.cache.git = fs.existsSync('.git');
-
-    if (this.cache.git === true) {
-      this.emit('git-clone');
-    }
-  }
-  return this.cache.git;
-};
 /**
  * Reads the package.json
  * @method loadClientJson
